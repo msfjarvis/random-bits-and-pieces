7f5b1d09ce4925d2dc0f4b1c3f809c134aa6aca7
Harsh Shandilya <harsh@prjkt.io>
2018-08-13 11:24:29 +0530

    scripts: Also add refactoring for material components
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- scripts/androidx-refactor.sh
+++ scripts/androidx-refactor.sh
@@ -6,10 +6,14 @@
 [ -f androidx-class-mapping.csv ] || wget https://developer.android.com/topic/libraries/support-library/downloads/androidx-class-mapping.csv
 sed -i '1d' androidx-class-mapping.csv # Remove the first line since it's not an actual class mapping
 
+[ -f material-class-mapping.csv ] || wget https://raw.githubusercontent.com/MSF-Jarvis/random-bits-and-pieces/master/scripts/material-class-mapping.csv
+
 parallel --bibtex # Praise the holy GNU
-for item in $(cat androidx-class-mapping.csv); do
-    orig=$(echo $item | cut -d ',' -f 1)
-    new=$(echo $item | cut -d ',' -f 2)
-    printf "Refactoring %s to %s\n" $orig $new
-    find app/src/main -type f | parallel -j+1 sed -i "s#$orig#$new#g" {} \;
+for mapping in androidx-class-mapping.csv material-class-mapping.csv; do
+    for item in $(cat "${mapping}"); do
+        orig=$(echo "${item}" | cut -d ',' -f 1)
+        new=$(echo "${item}" | cut -d ',' -f 2)
+        printf "Refactoring %s to %s\n" "${orig}" "${new}"
+        find app/src/main -type f | parallel -j+1 sed -i "s#$orig#$new#g" {} \;
+    done
 done

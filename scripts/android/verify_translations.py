abc6c64b539f3d7195dd402a3d2f449835944a11
Harsh Shandilya <msfjarvis@gmail.com>
2018-12-16 15:51:03 +0530

    verify_translations: Rewrite using argparse
    Allow specifying a tolerance percentage to check against
    before we consider a localization file too incomplete
    for usage.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- scripts/android/verify_translations.py
+++ scripts/android/verify_translations.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
+# Copyright (C) Harsh Shandilya <msfjarvis@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-only
+
 """
 Small script that quickly verifies if localizations
 for a given project are in sync with the source file
 """
 
-# Copyright (C) Harsh Shandilya <msfjarvis@gmail.com>
-# SPDX-License-Identifier: GPL-3.0-only
-
+import argparse
 import os
-import sys
 from xml.etree import ElementTree
 
 
@@ -26,11 +26,10 @@ def search_files(directory='.', file_name='strings.xml'):
     return output
 
 
-def analyze_translations():
+def analyze_translations(source_file, tolerance, remove_incomplete):
     """
     The site of the actual functioning of the script
     """
-    source_file = sys.argv[1]
     file_to_process = source_file.split("/")[-1]
     source = open(source_file, 'r').read()
     xml_root = ElementTree.fromstring(source)
@@ -41,17 +40,34 @@ def analyze_translations():
         source = open(found_file, 'r').read()
         xml_root = ElementTree.fromstring(source)
         strings = xml_root.findall('string')
-        if total_strings != len(strings):
-            print("%s is not fully in sync, missing %s strings"
+        percentage = (len(strings) / total_strings) * 100
+        if percentage < tolerance:
+            print("%s is not in sync with source files, missing %s strings"
                   % (found_file, total_strings - len(strings)))
-            if len(sys.argv) == 3 and sys.argv[2] == "remove":
+            if remove_incomplete:
+                print("Deleting")
                 os.remove(found_file)
         else:
-            print("%s is fully in sync with source files" % found_file)
+            print("%s in sync with source files" % found_file)
 
 
-if len(sys.argv) == 1:
-    print("Specify the source filename to begin")
-    sys.exit(1)
+def main():
+    """
+    Boilerplate for argument parsing
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--source", help="Source file to compare against; it's assumed "
+                                               "the localizations exist with the same file name.",
+                        type=str, default="app/src/main/res/values/strings.xml")
+    parser.add_argument("-t", "--tolerance", help="Percentage of missing strings that are "
+                                                  "allowed before we consider deletion",
+                        type=int, default=100)
+    parser.add_argument("-r", "--remove", help="Remove localizations not in the tolerance range",
+                        action="store_true")
+    args = parser.parse_args()
 
-analyze_translations()
+    analyze_translations(args.source, args.tolerance, args.remove)
+
+
+if __name__ == '__main__':
+    main()

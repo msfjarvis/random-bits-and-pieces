423b3779014a916399483c98f6201a8fa54c755e
Harsh Shandilya <msfjarvis@gmail.com>
2018-12-24 17:03:53 +0530

    scripts/android: Fixes for AndroidX refactor
    - Download scripts to /tmp
    - Allow overriding directory
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- scripts/android/androidx-refactor.sh
+++ scripts/android/androidx-refactor.sh
@@ -7,11 +7,11 @@
 # SC1117: Backslash is literal in "\n". Prefer explicit escaping: "\\n".
 
 if [[ ! -f androidx-class-mapping.csv ]]; then
-    wget https://developer.android.com/topic/libraries/support-library/downloads/androidx-class-mapping.csv
+    wget https://developer.android.com/topic/libraries/support-library/downloads/androidx-class-mapping.csv -O /tmp/androidx-class-mapping.csv
     sed -i '1d' androidx-class-mapping.csv # Remove the first line since it's not an actual class mapping
 fi
 
-[ -f material-class-mapping.csv ] || wget https://raw.githubusercontent.com/MSF-Jarvis/random-bits-and-pieces/master/scripts/android/material-class-mapping.csv
+[ -f material-class-mapping.csv ] || wget https://raw.githubusercontent.com/MSF-Jarvis/random-bits-and-pieces/master/scripts/android/material-class-mapping.csv -O /tmp/material-class-mapping.csv
 
 parallel --bibtex # Praise the holy GNU
 for mapping in androidx-class-mapping.csv material-class-mapping.csv; do
@@ -19,6 +19,6 @@ for mapping in androidx-class-mapping.csv material-class-mapping.csv; do
         orig=$(echo "${item}" | cut -d ',' -f 1)
         new=$(echo "${item}" | cut -d ',' -f 2)
         printf "Refactoring %s to %s\n" "${orig}" "${new}"
-        find app/src/main -type f | parallel -j+1 sed -i "s#$orig#$new#g" {} \;
-    done < "${mapping}"
+        find "${1:-app/src/main}" -type f | parallel -j+1 sed -i "s#$orig#$new#g" {} \;
+    done < "/tmp/${mapping}"
 done

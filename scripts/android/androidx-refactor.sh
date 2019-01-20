f856227dfc6a4e83fdcf2bbbb40c655944745312
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-21 00:50:02 +0530

    scripts/androidx-refactor: Fix some conditions
    Fixes: 423b3779014a ("scripts/android: Fixes for AndroidX refactor")
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- scripts/android/androidx-refactor.sh
+++ scripts/android/androidx-refactor.sh
@@ -6,12 +6,12 @@
 # shellcheck disable=SC1117
 # SC1117: Backslash is literal in "\n". Prefer explicit escaping: "\\n".
 
-if [[ ! -f androidx-class-mapping.csv ]]; then
+if [ ! -f /tmp/androidx-class-mapping.csv ]; then
     wget https://developer.android.com/topic/libraries/support-library/downloads/androidx-class-mapping.csv -O /tmp/androidx-class-mapping.csv
-    sed -i '1d' androidx-class-mapping.csv # Remove the first line since it's not an actual class mapping
+    sed -i '1d' /tmp/androidx-class-mapping.csv # Remove the first line since it's not an actual class mapping
 fi
 
-[ -f material-class-mapping.csv ] || wget https://raw.githubusercontent.com/MSF-Jarvis/random-bits-and-pieces/master/scripts/android/material-class-mapping.csv -O /tmp/material-class-mapping.csv
+[ -f /tmp/material-class-mapping.csv ] || wget https://raw.githubusercontent.com/MSF-Jarvis/random-bits-and-pieces/master/scripts/android/material-class-mapping.csv -O /tmp/material-class-mapping.csv
 
 parallel --bibtex # Praise the holy GNU
 for mapping in androidx-class-mapping.csv material-class-mapping.csv; do
